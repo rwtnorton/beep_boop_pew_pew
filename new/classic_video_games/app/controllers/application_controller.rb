e29627c234706d940b463989344c4ea3c5c71fb7
Richard W. Norton <rwtnorton@gmail.com>
2024-02-12 21:16:47 -0800

    update app controller with stuff from old

--- new/classic_video_games/app/controllers/application_controller.rb
+++ new/classic_video_games/app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
+lib = File.expand_path('../../../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
 class ApplicationController < ActionController::Base
+  protect_from_forgery
 end
